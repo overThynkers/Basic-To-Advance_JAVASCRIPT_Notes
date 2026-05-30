b98eeb12333493dae42b84080bc0c24914895ebe
overThynkers <workmailharoon@gmail.com>
2026-05-30 21:03:04 +0500

    Introduction to JavaScript Completed with Interview Questions

---
+++ 01_introduction/Introduction.js
@@ -0,0 +1,108 @@
+//! INTRODUCTION TO WEBSITE 
+
+//* How website works ?
+//? When user enter a website address URL (Uniform Resource Locator) in the browser, browser sends request to a web server. Web server checks and verifies the website address, if it exists then web server sends back required files like HTML, CSS, and JavaScript in response. Browser downloads these files and renders them on the screen.
+
+//* Building Blocks of Website 
+//? HTML - Makes basic structure of the website.  
+//? CSS - Adds styling and design to the website.  
+//? JavaScript - Adds functionality, interactivity and dynamic behavior to the website.
+
+//! INTRODUCTION TO JAVASCRIPT 
+
+//* What is JavaScript? 
+//? JavaScript - A programming language used to make websites interactive. It adds functionality like buttons, animations, form validation, and dynamic content updates on web pages.
+
+//* History of JavaScript 
+//? In 1995 - Created by Brendan Eich at Netscape in just 10 days.
+//? First name of javascript is (Mocha).
+//? Second Name of JavaSscript is (LiveScript).
+//? Last Name of JavaScript is (JavaScript) - Switching from livescript to javascript was a smart move to make it sound cooler and piggyback on Java's fame. while also cozying up to sun microsystems
+//? In 1996 JavaSscript was created.
+//? It was then submitted to ECMA International for standardization, which resulted in ECMAScript.
+//? In 2015 ES6/ ECMAScript 2015 Decided to switch to annual releases of ECMAScript.
+
+//* Ways to write JavaScript 
+//? Inline  JavaScript 
+//todo Exmaple <button onclick="alert('Hello')">CLick Me</button>
+
+//? Internal JavaSCript
+//todo Exmaple <script>console.log("Hellon World!")</script>
+
+//? External JavaScript 
+//todo Exmaple <script src="script.js"></script>
+
+
+
+
+// * 👇 External JavaScript
+console.log("This is External JS");
+
+
+//! INTRODUCTION TO WEBSITE (INTERVIEW QUESTIONS WITH ANSWERS)
+
+//* How does a website work?
+//? A website works when the browser sends a request to a web server and receives response files (HTML, CSS, JavaScript), which are then rendered on the screen.
+
+//* What happens when a user enters a URL in the browser?
+//? The browser sends an HTTP request to the server, the server processes it and returns the website files, which the browser displays.
+
+//* What is a web server?
+//? A web server is a computer/system that stores website files and delivers them to users when requested.
+
+//* Which files are sent from server to browser?
+//? HTML, CSS, and JavaScript files.
+
+//* What is the role of browser in website rendering?
+//? The browser reads HTML, CSS, and JavaScript and converts them into a visual webpage for the user.
+
+//* What are the building blocks of a website?
+//? HTML (structure), CSS (styling), JavaScript (functionality).
+
+//* What is HTML and why is it used?
+//? HTML is a markup language used to create the structure of a website.
+
+//* What is CSS and what is its purpose?
+//? CSS is used to style and design the website layout, colors, fonts, and spacing.
+
+//* What is JavaScript and why is it important?
+//? JavaScript is a programming language used to add interactivity and dynamic behavior to websites.
+
+
+//! INTRODUCTION TO JAVASCRIPT (INTERVIEW QUESTIONS WITH ANSWERS)
+
+//* What is JavaScript?
+//? JavaScript is a programming language used to make websites interactive and dynamic.
+
+//* Who created JavaScript and in which year?
+//? Brendan Eich created JavaScript in 1995 at Netscape.
+
+//* What was the original name of JavaScript?
+//? It was first called Mocha, then LiveScript, and finally JavaScript.
+
+//* Why was JavaScript renamed from LiveScript to JavaScript?
+//? It was renamed to gain popularity by associating it with Java.
+
+//* What is ECMAScript?
+//? ECMAScript is the standard specification on which JavaScript is based.
+
+//* When was ES6 introduced and why is it important?
+//? ES6 was introduced in 2015 and it added modern features like let/const, arrow functions, classes, etc.
+
+//* What are the different ways to write JavaScript?
+//? Inline, Internal, and External JavaScript.
+
+//* What is inline JavaScript?
+//? JavaScript written directly inside HTML elements using attributes like onclick.
+
+//* What is internal JavaScript?
+//? JavaScript written inside <script> tags in the same HTML file.
+
+//* What is external JavaScript?
+//? JavaScript written in a separate .js file and linked using <script src="file.js"></script>.
+
+//* Difference between inline, internal and external JavaScript?
+//? Inline is written in HTML elements, internal is inside HTML file, and external is in a separate file.
+
+//* Why is external JavaScript preferred over inline and internal?
+//? Because it improves code readability, reusability, and maintainability.
---
+++ all_Interview_questions.js
@@ -0,0 +1,37 @@
+//! INTRODUCTION TO JAVASCRIPT (INTERVIEW QUESTIONS WITH ANSWERS)
+
+//* What is JavaScript?
+//? JavaScript is a programming language used to make websites interactive and dynamic.
+
+//* Who created JavaScript and in which year?
+//? Brendan Eich created JavaScript in 1995 at Netscape.
+
+//* What was the original name of JavaScript?
+//? It was first called Mocha, then LiveScript, and finally JavaScript.
+
+//* Why was JavaScript renamed from LiveScript to JavaScript?
+//? It was renamed to gain popularity by associating it with Java.
+
+//* What is ECMAScript?
+//? ECMAScript is the standard specification on which JavaScript is based.
+
+//* When was ES6 introduced and why is it important?
+//? ES6 was introduced in 2015 and it added modern features like let/const, arrow functions, classes, etc.
+
+//* What are the different ways to write JavaScript?
+//? Inline, Internal, and External JavaScript.
+
+//* What is inline JavaScript?
+//? JavaScript written directly inside HTML elements using attributes like onclick.
+
+//* What is internal JavaScript?
+//? JavaScript written inside <script> tags in the same HTML file.
+
+//* What is external JavaScript?
+//? JavaScript written in a separate .js file and linked using <script src="file.js"></script>.
+
+//* Difference between inline, internal and external JavaScript?
+//? Inline is written in HTML elements, internal is inside HTML file, and external is in a separate file.
+
+//* Why is external JavaScript preferred over inline and internal?
+//? Because it improves code readability, reusability, and maintainability.
